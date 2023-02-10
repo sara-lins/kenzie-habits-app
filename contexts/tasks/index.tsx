efba11f773a0bb3a4aa8ae1e69c0db3ae9a97683
Sara Lins <saralins19@gmail.com>
2023-02-09 22:19:38 -0300

    feat: adding libs and changes

--- contexts/tasks/index.tsx
+++ contexts/tasks/index.tsx
@@ -7,19 +7,24 @@ interface ITasksProvider {
 
 interface IContext {
   homeState: string;
+  nameUser: string;
   setHomeState: Dispatch<SetStateAction<string>>;
+  setNameUser: Dispatch<SetStateAction<string>>;
 }
 
 export const TasksContext = createContext<IContext>({} as IContext);
 
 export const TasksProvider = ({ children }: ITasksProvider) => {
   const [homeState, setHomeState] = useState<string>("all");
+  const [nameUser, setNameUser] = useState<string>("User");
 
   return (
     <TasksContext.Provider
       value={{
         homeState,
+        nameUser,
         setHomeState,
+        setNameUser,
       }}
     >
       {children}
